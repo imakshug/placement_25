cd9762542e16936f131b3c8f801443680a25d4c3
iamakshug <akarni213@gmail.com>
2025-06-14 13:39:27 +0530

    june14

--- dailycode;)/maximum-difference-by-remapping-a-digit.py
+++ dailycode;)/maximum-difference-by-remapping-a-digit.py
@@ -1,13 +1,22 @@
 class Solution:
     def minMaxDifference(self, num: int) -> int:
         num_str = str(num)
-        max_digit = max(num_str)
-        min_digit = min(num_str)
 
-        # Create the maximum number by replacing the minimum digit with the maximum digit
-        max_num = int(num_str.replace(min_digit, max_digit))
-        
-        # Create the minimum number by replacing the maximum digit with '0'
-        min_num = int(num_str.replace(max_digit, '0'))
+        # Create max_num by replacing the first non-9 digit with 9
+        for c in num_str:
+            if c != '9':
+                max_num = int(num_str.replace(c, '9'))
+                break
+        else:
+            max_num = int(num_str)
+
+        # Create min_num by replacing the first digit with 0
+        first_char = num_str[0]
+        min_num = int(num_str.replace(first_char, '0'))
 
         return max_num - min_num
+
+# Example usage:
+solution = Solution()
+result = solution.minMaxDifference(123456)
+print(result)  # Output: 823456 -  23 456 = 800000
